c85d8dcb0aac5bfc4e96da463806e2fecc2f2c38
Billy Adan <mr.billyadan@gmail.com>
2023-09-20 08:08:04 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,62 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+/* domain variables */
+extern char **domains;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
+/* handle inherent */
+int apraiser(char **cmd, char *buf);
+void prompt_consumer(void);
+void handle_indication(int z);
+char **indicator(char *line);
+char *pathway_test(char **path, char *command);
+char *attach_path(char *path, char *command);
+int handle_inherent(char **command, char *line);
+void escape_cmd(char **command, char *line);
+
+void print_domain(void);
+
+/* string handlers */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void effect(char *cp, char **cmd);
+char *find_path(void);
+
+/* helper function for efficient free */
+void free_screen(char **buf);
+
+struct inherent
+{
+	char *domain;
+	char *escape;
+} inherent;
+
+struct info
+{
+	int final_escape;
+	int ln_count;
+} info;
+
+struct flags
+{
+	bool interchangeable;
+} flags;
+
+#endif
